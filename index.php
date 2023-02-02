edf38362afed791adab9734b9c2efec446293fe4
CodeSandbox <support@codesandbox.io>
2023-02-02 16:59:27 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>My PHP Website</title>
+</head>
+<body>
+<h1>My PHP Website</h1>
+<p style="font-size: 18px;">
+<?php
+$name = "CodeSandbox";
+echo "Hi! My name is " . $name . " 👋<br>";
+?>
+</p>
+</body>
+</html>
